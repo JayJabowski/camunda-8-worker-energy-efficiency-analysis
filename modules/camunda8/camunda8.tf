9ad86608199d9161da453c8a7d101d5dbbdf833b
Jabowski <jan.wittrowski@pm.me>
2023-10-31 17:28:38 +0100

    added fixed version to circumvent indent errs

--- modules/camunda8/camunda8.tf
+++ modules/camunda8/camunda8.tf
@@ -1,6 +1,7 @@
 resource "helm_release" "camunda-platform" {
   name       = "camunda-platform"
   chart      = "camunda-platform"
+  version    = "8.3.0"
   repository = "https://helm.camunda.io"
   namespace  = var.namespace
   count      = var.create_module ? 1 : 0
